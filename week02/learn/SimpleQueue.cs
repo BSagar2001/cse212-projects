4e53776c083ef0bc73cc7a2405f9d612813d7e68
BSagar2001 <sbhandari@byupathway.edu>
2026-07-08 12:13:55 +0530

    week02simpleQueue

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -10,7 +10,7 @@
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: Dequeue was using index 1 instead of index 0.
 
         Console.WriteLine("------------");
 
@@ -28,7 +28,7 @@
         Console.WriteLine(value);
         value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found: 
+        // Defect(s) Found: Enqueue was inserting at the front instead of adding to the back.
 
         Console.WriteLine("------------");
 
@@ -44,7 +44,7 @@
         catch (IndexOutOfRangeException) {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
+        // Defect(s) Found: None. The exception is thrown correctly.
     }
 
     private readonly List<int> _queue = new();
@@ -54,7 +54,8 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        // Add the new value to the back of the queue
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -66,8 +67,12 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        // Get the first item in the queue
+        var value = _queue[0];
+
+        // Remove the first item
+        _queue.RemoveAt(0);
+
         return value;
     }
 }
